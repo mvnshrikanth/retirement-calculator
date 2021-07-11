0de1a2abba4d3cb291a8e52ca1868c114f76f7f6
Shrikanth Malarouth <mvnshrikanth@gmail.com>
2021-07-11 16:09:03 +0530

    Added css classes
    Added css classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ExpenseCalculatorComponent } from './expense-calculator/expense-calculator.component';
 import { RetirementCorpusCalculatorComponent } from './retirement-corpus-calculator/retirement-corpus-calculator.component';
 import { MonthlyInvestmentsCalculatorComponent } from './monthly-investments-calculator/monthly-investments-calculator.component';
+import { AppNavigationComponent } from './app-navigation/app-navigation.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: ExpenseCalculatorComponent },
+  {
+    path: 'retirement-corpus-calc',
+    component: RetirementCorpusCalculatorComponent,
+  },
+  {
+    path: 'monthly-invst-calc',
+    component: MonthlyInvestmentsCalculatorComponent,
+  },
+  { path: '**', redirectTo:''},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ExpenseCalculatorComponent,
     RetirementCorpusCalculatorComponent,
-    MonthlyInvestmentsCalculatorComponent
+    MonthlyInvestmentsCalculatorComponent,
+    AppNavigationComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
