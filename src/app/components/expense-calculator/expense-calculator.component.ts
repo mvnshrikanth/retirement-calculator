904ee3c82e565e868966094874ddc472236d389d
Shrikanth Malarouth <mvnshrikanth@gmail.com>
2021-07-15 14:46:42 +0530

    Added floating class to divs
    Added floating class to divs

--- src/app/components/expense-calculator/expense-calculator.component.ts
+++ src/app/components/expense-calculator/expense-calculator.component.ts
@@ -10,19 +10,27 @@ import {UserData} from "../../modules/user-data";
 export class ExpenseCalculatorComponent implements OnInit {
 
   userData: UserData = {
-    curAnnualExp: 0,
-    curMonthlyExp: 0,
     annualInfRate: 0,
     curAge: 0,
+    curAnnualExp: 0,
+    curMonthlyExp: 0,
     expectedRetirementAge: 0,
-    numbOfYrsToRetire: 0,
-    expenseAtRetirement: 0
-  }
+    expenseAtRetirement: 0,
+    numbOfYrsToRetire: 0
+  };
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  disableButton: boolean = true;
+
+  enableSubmitButton() {
+    if (this.userData.curAnnualExp > 0 && (this.userData.annualInfRate > 0) && (this.userData.annualInfRate <= 15) && this.userData.numbOfYrsToRetire > 0) {
+      this.disableButton = false;
+    }
   }
 
   onSubmit(expenseCalc: NgForm) {
@@ -31,22 +39,25 @@ export class ExpenseCalculatorComponent implements OnInit {
 
   calcExpenseAtRetirement() {
     this.userData.expenseAtRetirement = Math.pow((1 + this.userData.annualInfRate / 100), this.userData.expectedRetirementAge) * this.userData.curAnnualExp;
+    this.enableSubmitButton();
   }
 
   calcAnnualExp() {
     this.userData.curAnnualExp = this.userData.curMonthlyExp * 12;
     this.calcExpenseAtRetirement();
+    this.enableSubmitButton();
   }
 
   calcMonthlyExp() {
     this.userData.curMonthlyExp = Number((this.userData.curAnnualExp / 12).toFixed(2));
     this.calcExpenseAtRetirement();
+    this.enableSubmitButton();
   }
 
   calcNumbOfYrsToRetire() {
     this.userData.numbOfYrsToRetire = this.userData.expectedRetirementAge - this.userData.curAge;
     this.calcExpenseAtRetirement();
+    this.enableSubmitButton();
   }
-
 
 }
