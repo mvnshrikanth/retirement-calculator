4f2a77bbc4f98d6f51d537e8befcf58152826311
Shrikanth Malarouth <mvnshrikanth@gmail.com>
2021-07-16 08:44:58 +0530

    Updated monthly investment and added validations
    Updated monthly investment and added validations for all the forms

--- src/app/components/expense-calculator/expense-calculator.component.ts
+++ src/app/components/expense-calculator/expense-calculator.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForm} from '@angular/forms';
 import {UserData} from "../../modules/user-data";
 
 @Component({
@@ -21,7 +20,9 @@ export class ExpenseCalculatorComponent implements OnInit {
     numOfYearsAftRetirement: 0,
     rateOfReturnAfterRetirement: 0,
     inflateAdjReturn: 0,
-    retirementCorpusAmount: 0
+    retirementCorpusAmount: 0,
+    rateOfInterestDuringAccumulation: 0,
+    monthlyInvestment: 0
   };
 
   constructor() {
@@ -30,35 +31,18 @@ export class ExpenseCalculatorComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  disableButton: boolean = true;
-
-  enableSubmitButton() {
-    if (this.userData.curAnnualExp > 0 && (this.userData.annualInfRate > 0) && (this.userData.annualInfRate <= 15) && this.userData.numbOfYrsToRetirement > 0) {
-      this.disableButton = false;
-    } else {
-      this.disableButton = true;
-    }
-  }
-
   calcAnnualExp() {
     this.userData.curAnnualExp = this.userData.curMonthlyExp * 12;
     this.calcExpenseAtRetirement();
-    this.enableSubmitButton();
   }
 
   calcNumbOfYrsToRetire() {
     this.userData.numbOfYrsToRetirement = this.userData.expectedRetirementAge - this.userData.curAge;
     this.calcExpenseAtRetirement();
-    this.enableSubmitButton();
   }
 
   calcExpenseAtRetirement() {
     this.userData.expenseAtRetirement = Math.pow((1 + this.userData.annualInfRate / 100), this.userData.numbOfYrsToRetirement) * this.userData.curAnnualExp;
-    this.enableSubmitButton();
-  }
-
-  onSubmit(expenseCalc: NgForm) {
-    console.log(expenseCalc.value, expenseCalc.valid);
   }
 
 }
--- src/app/components/retirement-corpus-calculator/retirement-corpus-calculator.component.ts
+++ src/app/components/retirement-corpus-calculator/retirement-corpus-calculator.component.ts
@@ -9,21 +9,21 @@ import {UserData} from "../../modules/user-data";
 export class RetirementCorpusCalculatorComponent implements OnInit {
 
   userData: UserData = {
-    curMonthlyExp: 60000,
+    curMonthlyExp: 10000,
     curAnnualExp: 720000,
     annualInfRate: 7,
     curAge: 30,
     expectedRetirementAge: 60,
     numbOfYrsToRetirement: 30,
-    expenseAtRetirement: 5480824,
+    expenseAtRetirement: 2740411,
     lifeExpectancy: 0,
     numOfYearsAftRetirement: 0,
     rateOfReturnAfterRetirement: 0,
     inflateAdjReturn: 0,
-    retirementCorpusAmount: 0
+    retirementCorpusAmount: 0,
+    rateOfInterestDuringAccumulation: 0,
+    monthlyInvestment: 0
   };
-
-  disableButton: boolean = true;
 
   constructor() {
   }
@@ -31,28 +31,17 @@ export class RetirementCorpusCalculatorComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  enableSubmitButton() {
-    if (this.userData.numOfYearsAftRetirement > 0 && this.userData.rateOfReturnAfterRetirement > 0) {
-      this.disableButton = false;
-    } else {
-      this.disableButton = true;
-    }
-  }
-
   calcNumOfYearsAftRetirement() {
     this.userData.numOfYearsAftRetirement = this.userData.lifeExpectancy - this.userData.expectedRetirementAge;
     this.calcRetirementCorpusAmount();
-    this.enableSubmitButton();
   }
 
   calcInflationAdjustedReturn() {
     this.userData.inflateAdjReturn = (((1 + this.userData.rateOfReturnAfterRetirement) / (1 + this.userData.annualInfRate)) - 1) * 100;
     this.calcRetirementCorpusAmount();
-    this.enableSubmitButton();
   }
 
   calcRetirementCorpusAmount() {
     this.userData.retirementCorpusAmount = this.userData.expenseAtRetirement * this.userData.numOfYearsAftRetirement * this.userData.inflateAdjReturn;
-    this.enableSubmitButton();
   }
 }
